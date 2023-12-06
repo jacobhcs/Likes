c69a665018c4a61f525bdf9a3a715807f23d8995
jacobhcs <jaharr1999@gmail.com>
2023-12-06 18:32:24 -0500

    lazy solution

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ function addLike() {
   count ++;
   likeTrack.innerText = count + " like(s)";
 }
+
+var likeTrack2 = document.querySelector('#like-count2');
+
+var count2 = 12;
+
+function addLike2() {
+  count2 ++;
+  likeTrack2.innerText = count2 + " like(s)";
+}
+
+
+var likeTrack3 = document.querySelector('#like-count3');
+
+var count3 = 9;
+
+function addLike3() {
+  count3 ++;
+  likeTrack3.innerText = count3 + " like(s)";
+}
